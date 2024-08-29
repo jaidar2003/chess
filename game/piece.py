e72bae325035faa95bcc2339c04a997c61335b1f
Juan Manuel Aidar <j.aidar@alumno.um.edu.ar>
2024-08-29 08:31:36 -0300

    -

--- game/piece.py
+++ game/piece.py
@@ -1,51 +1,97 @@
 class Piece:
     def __init__(self, color):
-        self.color = color
+        self.__color__ = color
+        self.__type__ = None
 
-    def valid_move(self, origin, destination, board):
-        raise NotImplementedError("Este método debe ser implementado en una subclase.")
+    def show(self):
+        symbols = {
+            "ROOK": {"WHITE": "♖", "BLACK": "♜"},
+            "PAWN": {"WHITE": "♙", "BLACK": "♟"},
+            "KNIGHT": {"WHITE": "♘", "BLACK": "♞"},
+            "BISHOP": {"WHITE": "♗", "BLACK": "♝"},
+            "QUEEN": {"WHITE": "♕", "BLACK": "♛"},
+            "KING": {"WHITE": "♔", "BLACK": "♚"}
+        }
+        return symbols[self.__type__][self.__color__]
 
-    def __str__(self):
-        raise NotImplementedError("Este método debe ser implementado en una subclase.")
-
-class King(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def __str__(self):
-        return 'K' if self.color == 'white' else 'k'
-
-class Queen(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def __str__(self):
-        return 'Q' if self.color == 'white' else 'q'
-
-class Bishop(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def __str__(self):
-        return 'B' if self.color == 'white' else 'b'
-
-class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def __str__(self):
-        return 'N' if self.color == 'white' else 'n'
 
 class Rook(Piece):
     def __init__(self, color):
         super().__init__(color)
+        self.__type__ = "ROOK"
 
-    def __str__(self):
-        return 'R' if self.color == 'white' else 'r'
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        if to_row == from_row and to_col != from_col:
+            return True
+        elif to_col == from_col and to_row != from_row:
+            return True
+        return False
+
 
 class Pawn(Piece):
     def __init__(self, color):
         super().__init__(color)
+        self.__type__ = "PAWN"
 
-    def __str__(self):
-        return 'P' if self.color == 'white' else 'p'
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        direction = -1 if self.__color__ == "WHITE" else 1
+        if to_col == from_col:  # Movimiento hacia adelante
+            if (to_row - from_row) == direction and board.get_piece(to_row, to_col) == "No piece":
+                return True
+            if (from_row == 6 and self.__color__ == "WHITE") or (from_row == 1 and self.__color__ == "BLACK"):
+                if (to_row - from_row) == 2 * direction and board.get_piece(to_row, to_col) == "No piece":
+                    return True
+        if abs(to_col - from_col) == 1 and (to_row - from_row) == direction:
+            destination_piece = board.get_piece(to_row, to_col)
+            if destination_piece != "No piece" and destination_piece[1] != self.__color__:
+                return True
+        return False
+
+
+class Knight(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.__type__ = "KNIGHT"
+
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        valid_moves = [
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)]
+        return (to_row - from_row, to_col - from_col) in valid_moves
+
+
+class Bishop(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.__type__ = "BISHOP"
+
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        if abs(to_row - from_row) == abs(to_col - from_col):
+            return True
+        return False
+
+
+class Queen(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.__type__ = "QUEEN"
+
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        if to_row == from_row and to_col != from_col:
+            return True
+        elif to_col == from_col and to_row != from_row:
+            return True
+        elif abs(to_row - from_row) == abs(to_col - from_col):
+            return True
+        return False
+
+
+class King(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.__type__ = "KING"
+
+    def permited_move(self, from_row, from_col, to_row, to_col, board):
+        if abs(from_row - to_row) <= 1 and abs(from_col - to_col) <= 1 and not (from_row == to_row and from_col == to_col):
+            return True
+        return False
--- test/piece_test.py
+++ test/piece_test.py
@@ -2,18 +2,7 @@ import unittest
 from game.piece import King, Rook, Pawn
 
 class TestPiece(unittest.TestCase):
-
-    def test_king(self):
-        king = King("white")
-        self.assertEqual(str(king), 'K')
-
-    def test_rook(self):
-        rook = Rook("black")
-        self.assertEqual(str(rook), 'r')
-
-    def test_pawn(self):
-        pawn = Pawn("white")
-        self.assertEqual(str(pawn), 'P')
+    pass
 
 if __name__ == '__main__':
     unittest.main()
