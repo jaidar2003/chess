c9cade20d27546a9bb32c84762e20be7c601aed4
Juan Manuel Aidar <j.aidar@alumno.um.edu.ar>
2024-08-30 21:48:09 -0300

    -

--- test/piece_test.py
+++ test/piece_test.py
@@ -1,28 +1,29 @@
 import unittest
 from game.piece import *
 
-def test_rook_white(self):
-    piece = Piece("white")
-    piece._Piece__type__ = "ROOK"
-    self.assertEqual(piece.show(), "♖")
+class TestPiece(unittest.TestCase):
+    def test_rook_white(self):
+        piece = Piece("white")
+        piece._Piece__type__ = "ROOK"
+        self.assertEqual(piece.show(), "♖")
 
 
-def test_rook_black(self):
-    piece = Piece("black")
-    piece._Piece__type__ = "ROOK"
-    self.assertEqual(piece.show(), "♜")
+    def test_rook_black(self):
+        piece = Piece("black")
+        piece._Piece__type__ = "ROOK"
+        self.assertEqual(piece.show(), "♜")
 
 
-def test_queen_white(self):
-    piece = Piece("white")
-    piece._Piece__type__ = "QUEEN"
-    self.assertEqual(piece.show(), "♕")
+    def test_queen_white(self):
+        piece = Piece("white")
+        piece._Piece__type__ = "QUEEN"
+        self.assertEqual(piece.show(), "♕")
 
 
-def test_queen_black(self):
-    piece = Piece("black")
-    piece._Piece__type__ = "QUEEN"
-    self.assertEqual(piece.show(), "♛")
+    def test_queen_black(self):
+        piece = Piece("black")
+        piece._Piece__type__ = "QUEEN"
+        self.assertEqual(piece.show(), "♛")
 
 if __name__ == "__main__":
     unittest.main()
